705e677b7236f9b3f1aea79df81d12a848510faf
AstraKlaus <endrew-k@yandex.ru>
2022-12-04 23:57:08 +0400

    create person ability @ModelAttribute more functions

--- src/main/java/ak/spring/controllers/PeopleController.java
+++ src/main/java/ak/spring/controllers/PeopleController.java
@@ -2,12 +2,9 @@ package ak.spring.controllers;
 
 import ak.spring.dao.PersonDAO;
 import ak.spring.models.Person;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -29,5 +26,16 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model){
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
