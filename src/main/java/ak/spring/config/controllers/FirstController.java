e77ada0002f4e1806669fcbf244183a70642bd10
AstraKlaus <endrew-k@yandex.ru>
2022-12-04 14:09:52 +0400

    add new page with calculator

--- src/main/java/ak/spring/config/controllers/FirstController.java
+++ src/main/java/ak/spring/config/controllers/FirstController.java
@@ -13,19 +13,17 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname,
-                            Model model){
-
-        //System.out.println("Hello, " + name + " " + surname);
-
-        model.addAttribute("message", "Hello, " + name + surname);
-
+    public String helloPage(@RequestParam(value = "name", required = false) String first,
+                            @RequestParam(value = "surname", required = false) String second,
+                            Model model) {
+        model.addAttribute("greetings", "Hello, " + first + " " + second);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage() {
         return "first/goodbye";
     }
+
+
 }
